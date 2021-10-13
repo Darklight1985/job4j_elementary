d3f5b97a670ecd3e62f08aa07b80c8f93d20606b
Dmitrij Kolesnev <darklight1985@mail.ru>
2021-10-13 20:05:16 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -7,10 +7,17 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Dmitriy Kolesnev";
+        names[1] = "Yurij Gagarin";
+        names[2] = "Bill Gejts";
+        names[3] = "Ilon Mask";
 
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
-
+        for (String s: names) {
+            System.out.println(s);
+        }
     }
 }
